80dd04415fdad185a0f5fa55dd5132f461d60520
jamos90 <jamosamos90@gmail.com>
2023-03-14 20:15:39 -0400

    adds player and colliders with platform to map

--- src/scenes/Preload.js
+++ src/scenes/Preload.js
@@ -8,8 +8,9 @@ class Preload extends Phaser.Scene {
 
   preload() {
     this.load.tilemapTiledJSON('crystalWorld', 'assets/crystal_world_map.json');
-    this.load.image('tileSetOne', 'assets/main_lev_build_1.png')
-    this.load.image('tileSetTwo', 'assets/main_lev_build_2.png')
+    this.load.image('tileSetOne', 'assets/main_lev_build_1.png');
+    this.load.image('tileSetTwo', 'assets/main_lev_build_2.png');
+    this.load.image('player', 'assets/player/movements/idle01.png');
   }
 
   create() {
